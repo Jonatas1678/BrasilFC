14dd6b87c8d4c71b51473aa87869eab6fbc3b57d
Jonatas Lima Barbosa <67673863+Jonatas1678@users.noreply.github.com>
2025-04-30 00:26:54 -0300

    Update script.js

--- Js/script.js
+++ Js/script.js
@@ -5,17 +5,19 @@ console.log(
     titulo1
 );
 
- titulo1.innerHTML = 'Hinos dos principais clubes brasileiros!'
+titulo1.innerHTML = 'Hinos dos principais clubes brasileiros!'
 
 console.log(titulo1.innerHTML);
 
-titulo1.style.color = 'blue'
+titulo1.style.background = 'linear-gradient(to right, #FFD700, #008000, #0000FF)'
+titulo1.style.backgroundClip = 'text';
+titulo1.style.color = 'transparent';
 
 
-document.getElementsByClassName('') 
+document.getElementsByClassName('')
 
 let cadastrar = document.querySelector('#idCadastrar')
 
- cadastrar.addEventListener('submit', ()=>{
-     alert("se cadastrar")
-     })
+cadastrar.addEventListener('submit', () => {
+    alert("se cadastrar")
+})
